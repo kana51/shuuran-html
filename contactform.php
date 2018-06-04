45399e594d5dbe20105102c1178409820597ad3d
tachiyama kana <tachiyama@kana-tachiyama.local>
2018-06-04 13:11:46 +0900

    0604

---
+++ contactform.php
@@ -0,0 +1,211 @@
+<?php
+//データ取得
+$name = isset($_POST["name"])? $_POST["name"]: "";
+$hira = isset($_POST["hira"])? $_POST["hira"]: "";
+$zip11 = isset($_POST["zip11"])? $_POST["zip11"]: "";
+$addr11 = isset($_POST["addr11"])? $_POST["addr11"]: "";
+$tel = isset($_POST["tel"])? $_POST["tel"]: "";
+$fax = isset($_POST["fax"])? $_POST["fax"]: "";
+$email = isset($_POST["email"])? $_POST["email"]: "";
+$selece = isset($_POST["select"])? $_POST["select"]: "";
+$request = isset($_POST["request"])? $_POST["request"]: "";
+
+//画面表示
+?>
+
+
+
+<!doctype html>
+<html lang="jp">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>その他お問い合わせ | Official Site</title>
+  <link href="https://fonts.googleapis.com/earlyaccess/sawarabimincho.css" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="responsive.css">
+  <link rel="stylesheet" href="pagestyle.css">
+  <link rel="stylesheet" href="orderform.css">
+  <script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
+</head>
+<body>
+  <header>
+    <!-- pc-only -->
+    <div class="pc-only">
+      <div class="container">
+        <div class="icon">
+          <a href="index.html"><img src="pics/logo.png" id="logo"></a>
+        </div>
+        <div class="menu">
+          <ul>
+            <li><a href="about.html" class="about">About</a></li>
+            <li><a href="gallery.html" class="gallery">Gallery</a></li>
+            <li><a href="school.html" class="school">School</a></li>
+            <li><a href="ordermade.html" class="ordermade">Order Made</a></li>
+            <li><a href="index.html#contact" class="contact">Contact</a></li>
+          </ul>
+        </div>
+      </div><!-- end container -->
+    </div><!-- end pc-only -->
+
+
+    <!-- sp-only -->
+    <div class="sp-only">
+      <nav class="navbar navbar-default navbar-fixed-top">
+        <!-- nav-header -->
+        <div class="navbar-header">
+            <!-- logo -->
+          <a href="index.html"><img src="pics/logo.png" id="logo-img"></a>
+            <!-- toggle -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#top-nav">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div><!-- end nav-header -->
+        <!-- top menu -->
+        <div class="collapse navbar-collapse" id="top-nav">
+            <!-- left -->
+            <ul class="nav navbar-nav">
+              <li><a href="about.html" class="about">About</a></li>
+              <li><a href="gallery.html" class="gallery">Gallery</a></li>
+              <li><a href="school.html" class="school">School</a></li>
+              <li><a href="ordermade.html" class="ordermade">Order Made</a></li>
+              <li><a href="index.html#contact" class="contact">Contact</a></li>
+            </ul>
+        </div><!-- end top menu -->
+      </nav><!-- end nav -->
+    </div><!-- end sp-only -->
+  </header>
+
+<!-- order-form -->
+  <div class="content-wrapper">
+    <div class="container">
+      <div class="content-title">
+        <h4>その他のお問い合わせ</h4>
+      </div>
+    </div><!-- end container -->
+
+    <div class="contact-container">
+      <div class="mod-dl-table">
+        <form name="form1" action="./contactconform.php" method="post">
+            <dl>
+            <h5>お客様情報をご記入ください</h5>
+            <!-- お名前 -->
+            <dt>お名前 <span class="mod-icon-inquiry">必須</span></dt>
+            <dd>
+              <input type="text" name="name" value="<?php echo htmlspecialchars($name, ENT_QUOTES, "utf-8"); ?>" id="input-name" required>
+            </dd>
+
+            <!-- おなまえ -->
+            <dt>おなまえ<span class="mod-icon-inquiry">必須</span></dt>
+            <dd>
+              <input type="text" name="hira" value="<?php echo htmlspecialchars($hira, ENT_QUOTES, "utf-8"); ?>" id="input-name" required>
+            </dd>
+
+            <!-- 郵便番号 -->
+            <dt>郵便番号<span class="mod-icon-inquiry">必須</span></dt>
+            <dd>
+              <input type="text" name="zip11" size="10" maxlength="8" onKeyUp="AjaxZip3.zip2addr(this,'','addr11','addr11');" value="<?php echo htmlspecialchars($zip11, ENT_QUOTES, "utf-8"); ?>"  placeholder="(例)1230001" id="zip" required>
+            </dd>
+
+            <!-- 住所 -->
+            <dt>ご住所<span class="mod-icon-inquiry">必須</span></dt>
+            <dd>
+              <input type="text" name="addr11" size="60" value="<?php echo htmlspecialchars($addr11, ENT_QUOTES, "utf-8"); ?>" id="input-address" required>
+            </dd>
+
+            <!-- 電話番号 -->
+            <dt>電話番号<span class="mod-icon-inquiry">必須</span></dt>
+            <dd>
+              <input type="tel" name="tel" value="<?php echo htmlspecialchars($tel, ENT_QUOTES, "utf-8"); ?>" id="input-tel" placeholder="(例)08012345678" required>
+            </dd>
+
+            <!-- FAX番号 -->
+            <dt>FAX番号</dt>
+            <dd>
+              <input type="tel" name="fax" value="<?php echo htmlspecialchars($fax, ENT_QUOTES, "utf-8"); ?>" id="input-fax" placeholder="(例)08012345678">
+            </dd>
+
+            <!-- メールアドレス -->
+            <dt>メールアドレス<span class="mod-icon-inquiry">必須</span></dt>
+            <dd>
+              <input type="email" name="email" value="<?php echo htmlspecialchars($email, ENT_QUOTES, "utf-8"); ?>" id="input-mail" required>
+            </dd>
+          </dl>
+          <dl>
+            <h5>お問い合わせ内容についてご記入ください</h5>
+
+            <!-- ご希望・ご依頼クラス -->
+            <dt>お問い合わせ種目<span class="mod-icon-inquiry">必須</span></dt>
+            <dd>
+              <select class="custom-select my-1 mr-sm-2" name="select" id="inlineFormCustomSelectPref" value="<?php echo htmlspecialchars($select, ENT_QUOTES, "utf-8"); ?>" required>
+                <option selected>選択してください</option>
+                <option value="商品について">商品について</option>
+                <option value="当サイトについて">当サイトについて</option>
+                <option value="その他">その他</option>
+              </select>
+            </dd>
+
+
+            <!-- 要望・質問 -->
+            <dt>お問い合わせ内容</dt>
+            <dd>
+              <textarea name="request" placeholder="お問い合わせ内容をご記入ください。" required><?php echo htmlspecialchars($request, ENT_QUOTES, "utf-8"); ?></textarea>
+            </dd>
+          </dl>
+          <input type="submit" name="next_btn" value="Submit" class="submit">
+        </form>
+      </div><!-- end mod-dl-table -->
+    </div><!-- end contact-container -->
+  </div><!-- end content-wrapper -->
+
+
+
+
+
+
+
+  <p id="pageTop"><a href="#"><i class="fa fa-chevron-up"></i></a></p>
+
+  <!-- footer -->
+  <footer>
+    <div class="container">
+      <div class="footer-left">
+        <a herf="index.html"><img src="pics/logo.png" id="footer-logo"></a>
+        <ul class="shop-info">
+          <li class="time">10:00 〜 19:00</li>
+          <li class="tell">TEL：03−1234−5678</li>
+          <li>お問合せは<a href="#" class="contact-link-footer">こちら</a></li>
+        </ul>
+        <div class="footer-icon sp-only">
+          <p>＼ Follow Me!! ／</p>
+          <ul class="footer-sns">
+            <li><a href="#" id="fb-footer"><i class="fab fa-facebook-f"></i></i></a></li>
+            <li><a href="#" id="tw-footer"><i class="fab fa-twitter"></i></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-right pc-only">
+        <ul class="footer-menu">
+          <li><a href="about.html" class="about">About</a></li>
+          <li><a href="gallery.html" class="gallery">Gallery</a></li>
+          <li><a href="school.html" class="school">School</a></li>
+          <li><a href="ordermade.html" class="ordermade">Order Made</a></li>
+          <li><a href="index.html#contact" class="contact">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+
+
+   <!-- script -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
---
+++ success.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>送信完了</title>
+</head>
+<body>
+    <h1>メール送信が完了しました。</h1>
+    <p><a href="test.php">TOPに戻る</a></p>
+</body>
+</html>
